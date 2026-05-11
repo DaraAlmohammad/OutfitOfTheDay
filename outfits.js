3ff0298acef67c5110d2b9470c704cf07cfac297
00Darios9 <daraalmohammad@gmail.com>
2026-05-11 16:22:47 +0200

    fixade funktionen getAlloutfits

--- outfits.js
+++ outfits.js
@@ -1,16 +1,25 @@
-export function getAllOutfits() {
+class Outfits{
+
+     static async getAllOutfits() {
+        try{
+            let response = await fetch("/mainPage");
+            let jsonData = await response.json();
+            return jsonData.outfits;
+        } catch(err){
+            console.log("err")
+        }
+        }
+  
+
+    static deleteProduct(id){
         let data = deno.readTextFilesync("data.json");
         let newData = JSON.parse(data);
-        return newData.outfits;
-    }
-export function deleteProduct(id){
-    let data = deno.readTextFilesync("data.json");
-    let newData = JSON.parse(data);
-    for (let i = 0; i < newData.outfits.length; i++){
-        if(newData.outfits[i].id == id){
-            newData.products.splice(i,1);
+        for (let i = 0; i < newData.outfits.length; i++){
+            if(newData.outfits[i].id == id){
+                newData.outfits.splice(i,1);
+            }
         }
+        let finelData = JSON.stringify(newData);
+        Deno.writeTextFileSync("data.json",finelData)
     }
-    let finelData = JSON.stringify(newData);
-    Deno.writeTextFileSync("data.json",finelData)
 }
--- server.js
+++ server.js
@@ -2,30 +2,39 @@ import { serveDir } from "jsr:@std/http/file-server";
 
 async function handle(request) {
     let url = new URL(request.url);
-    if (url.pathname.startsWith("/login.html")) {
-        return serveDir(request,{fsRoot:"."});
-    }
-    if (url.pathname == "/login" && url.method=="POST") {
-        console.log(request)
-        let user = await request.json();
+    if (url.pathname === "/mainPage" && request.method === "GET") {
+    
+    let data = await Deno.readTextFile("data.json");
+    return new Response(data, {
+        headers: { "Content-Type": "application/json" }
+    });
+}
 
-        let data = Deno.readTextFile("users.json");
-        JSON.stringify(data);
-        for (let element of data) {
-            if (element.username == user.username && element.password == user.password) {
+if (url.pathname.startsWith("/login.html")) {
+    return serveDir(request,{fsRoot:"."});
+}
 
-                let options = {
-                    headers: {
-                        "Set-Cookie": `session_id=${crypto.randomUUID()}; Max-Age=86400`
-                    }
+if (url.pathname == "/login" && url.method=="POST") {
+    console.log(request)
+    let user = await request.json();
+    
+    let data = Deno.readTextFile("users.json");
+    JSON.stringify(data);
+    for (let element of data) {
+        if (element.username == user.username && element.password == user.password) {
+            
+            let options = {
+                headers: {
+                    "Set-Cookie": `session_id=${crypto.randomUUID()}; Max-Age=86400`
                 }
-
             }
+            
         }
-        console.log(crypto.randomUUID())
-        
-        return new Response("Welcome!", options)
     }
-    return new Response("Hej")
+    console.log(crypto.randomUUID())
+    
+    return new Response("Welcome!", options)
+}
+return serveDir(request, { fsRoot: "." });
 }
 Deno.serve(handle);
