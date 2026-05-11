4bcc758c603653b73a594788209097bcbef495f7
00Darios9 <daraalmohammad@gmail.com>
2026-05-11 14:39:04 +0200

    Update outfits.js

--- outfits.js
+++ outfits.js
@@ -3,4 +3,14 @@ export function getAllOutfits() {
         let newData = JSON.parse(data);
         return newData.outfits;
     }
-
+export function deleteProduct(id){
+    let data = deno.readTextFilesync("data.json");
+    let newData = JSON.parse(data);
+    for (let i = 0; i < newData.outfits.length; i++){
+        if(newData.outfits[i].id == id){
+            newData.products.splice(i,1);
+        }
+    }
+    let finelData = JSON.stringify(newData);
+    Deno.writeTextFileSync("data.json",finelData)
+}
