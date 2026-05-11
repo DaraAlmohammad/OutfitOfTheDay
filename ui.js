097a83d23ef2a5e40362aa14db7183a0315bebe4
00Darios9 <daraalmohammad@gmail.com>
2026-05-11 16:30:23 +0200

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,30 @@
+async function createAllOutfits() {
+    let outfitList = document.getElementById("outfit-feed-container");
+    outfitList.innerHTML = "";
+    let allOutfits = await Outfits.getAllOutfits();
+    console.log(allOutfits);
+
+    for (let outfit of allOutfits) {
+        let a = document.createElement("a");
+        
+        if (outfit.isFavourite === true) {
+            a.innerHTML = `
+            <img src="${outfit.image}">
+            <img src="images/favorite.jpg">
+        `
+        } else {
+            a.innerHTML = `
+            <img src="${outfit.image}">
+            <img src="images/NotAfavorite.jpg">
+        `
+        }
+        outfitList.appendChild(a);
+    }
+}
+  static async function getOutfitByfilter(){
+        const allOutfits = Outfits.getAllOutfits();
+        
+
+    }
+
+createAllOutfits();
