ad7a53284562e79f5c6006f4de8ff1968a061bbc
00Darios9 <daraalmohammad@gmail.com>
2026-05-11 17:44:08 +0200

    Update ui.js

--- ui.js
+++ ui.js
@@ -21,5 +21,21 @@ async function createAllOutfits() {
         outfitList.appendChild(a);
     }
 }
+const form = document.querySelector("#filter-form");
+form.addEventListener("submit", async function (){
+    const outfits = Outfits.getAllOutfits();
+    const form = document.querySelector("#filter-form");
+    const color = form.elements.color.value;
+
+    let filterdOutfits = [];
+    for(let element of outfits){
+        if(element.color == color){
+            filterdOutfits.push(element);
+        }
+    }
+    return filterdOutfits;
+
+})
+
 
 createAllOutfits();
