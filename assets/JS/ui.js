5e0ac1273c72295c64abee7b2e7d1ae1c185a003
RuthGranath <ruth.granath@yahoo.com>
2026-05-12 10:17:30 +0200

    lite-ändringar

--- assets/JS/ui.js
+++ assets/JS/ui.js
@@ -14,9 +14,9 @@ class UI {
 
                 let heartSrc = "";
                 if (outfit.isFavourite === true) {
-                    heartSrc = "images/favorite.jpg";
+                    heartSrc = "assets/images/favorite.jpg";
                 } else {
-                    heartSrc = "images/NotAfavorite.jpg";
+                    heartSrc = "assets/images/NotAfavorite.jpg";
                 }
 
                 card.innerHTML = `
@@ -86,5 +86,5 @@ class UI {
         `
     }
 }
-const ui = new UI();
-ui.createAllOutfits();
+
+UI.createAllOutfits();
