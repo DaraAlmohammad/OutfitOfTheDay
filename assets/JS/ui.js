1c1192d45696865c3fe77ab593af0ffe94528baf
00Darios9 <daraalmohammad@gmail.com>
2026-05-12 09:44:37 +0200

    updaterade ui med att ta bort en addeventlistner

--- assets/JS/ui.js
+++ assets/JS/ui.js
@@ -86,22 +86,4 @@ class UI {
         `
     }
 }
-const form = document.querySelector("#filter-form");
-form.addEventListener("submit", async function (){
-    const outfits = Outfits.getAllOutfits();
-    const form = document.querySelector("#filter-form");
-    const color = form.elements.color.value;
-
-    let filterdOutfits = [];
-    for(let element of outfits){
-        if(element.color == color){
-            filterdOutfits.push(element);
-        }
-    }
-    return filterdOutfits;
-
-})
-
-
-
 UI.createAllOutfits;
