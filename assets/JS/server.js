c84fd4dc92abd3a144eba2110c04bb27464fea65
tovaadolfsson <tova.adolfsson@hotmail.com>
2026-05-12 10:33:07 +0200

    Update server.js

--- assets/JS/server.js
+++ assets/JS/server.js
@@ -1,5 +1,5 @@
 import { serveDir } from "jsr:@std/http/file-server";
-import { getOutfitById } from "./outfits.js";
+import { getOutfitById,} from "./outfits.js";
 
 async function handle(request) {
     let url = new URL(request.url);
